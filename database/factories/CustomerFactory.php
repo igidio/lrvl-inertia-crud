f46c5a0b9c4b00000211391aac651e6a09ca612b
igidio <61283392+igidio@users.noreply.github.com>
2024-06-16 15:47:53 -0400

    fix(customer): generate realistic dates from factory

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -19,12 +19,15 @@ class CustomerFactory extends Factory
     public function definition(): array
     {
 
+        $firstName = $this->faker->firstName;
+        $lastName = $this->faker->lastName;
+        $number = rand(1, 100);
         #$this->faker = \Faker\Factory::create('es_AR');
         return [
-            'nombres' => $this->faker->firstName,
-            'apellidos' => $this->faker->lastName,
+            'nombres' => $firstName,
+            'apellidos' => $lastName,
             'ci' => $this->faker->unique()->numberBetween(10000000, 19999999),
-            'email' => $this->faker->unique()->safeEmail,
+            'email' => strtolower($firstName) . '.' . strtolower($lastName) . $number . '@example.com',
             'telefono' => $this->faker->unique()->numberBetween(70000000, 79999999),
             'direccion' => $this->faker->address,
             'fecha_nacimiento' => $this->faker->dateTimeBetween('-60 year', '-18 year'),
