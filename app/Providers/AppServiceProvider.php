e3c33437e9a119e2800fcbf8db2eb383a15e5f1b
Salvador Cáceres C. <salvadorcaceresc@gmail.com>
2025-06-25 00:30:08 -0400

    minor fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
         Inertia::share('flash', function () {
             return ['status' => Session::get('status')];
         });
